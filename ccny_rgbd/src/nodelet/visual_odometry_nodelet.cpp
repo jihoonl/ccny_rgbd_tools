d84bee02371186621967951ecbb26ad2d80dc56b
Vladyslav Usenko <vsu1991@gmail.com>
2013-05-17 16:26:05 +0200

    added nodelet version of visual odometry

---
+++ ccny_rgbd/src/nodelet/visual_odometry_nodelet.cpp
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2013, City University of New York
+ *  Ivan Dryanovski <ivan.dryanovski@gmail.com>
+ *
+ *  CCNY Robotics Lab
+ *  http://robotics.ccny.cuny.edu
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ccny_rgbd/nodelet/visual_odometry_nodelet.h"
+
+namespace ccny_rgbd {
+
+PLUGINLIB_DECLARE_CLASS(ccny_rgbd, VisualOdometryNodelet, VisualOdometryNodelet, nodelet::Nodelet);
+
+void VisualOdometryNodelet::onInit()
+{
+  NODELET_INFO("Initializing RGBD Image Proc Nodelet");
+  
+  // TODO: Do we want the single threaded or multithreaded NH?
+  ros::NodeHandle nh         = getMTNodeHandle();
+  ros::NodeHandle nh_private = getMTPrivateNodeHandle();
+
+  visual_odometry_ = new VisualOdometry(nh, nh_private);
+}
+
+} // namespace ccny_rgbd
